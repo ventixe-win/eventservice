65eb744c07e3e2d1198a3ae512e7f54ba0639e71
Albin <albinjorgensen@hotmail.com>
2025-05-09 17:20:08 +0200

    Add project files.

---
+++ WebApi/Controllers/EventsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Services;
+
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EventsController(IEventService eventService) : ControllerBase
+{
+    private readonly IEventService _eventService = eventService;
+}
